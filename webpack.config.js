4f4a6ec2e47e3469b1aa9c549be66ce1e6cf0c58
Moises Mariano <moisesduartem@gmail.com>
2020-09-07 12:16:46 -0300

    feat: mode define a minificacao

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
+const mode = process.env.NODE_ENV;
+const modeDev = mode !== 'production';
+const jsExtension = (modeDev) ? '.js' : '.min.js';
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: mode,
     entry: './src/index.js',
     output: {
-        filename: 'bundle.min.js',
+        filename: 'bundle' + jsExtension,
         path: __dirname + '/public/'
     },
     plugins: [

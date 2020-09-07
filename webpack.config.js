e0332de0fd061adb69e54ace77ed52e754d92b27
Moises Mariano <moisesduartem@gmail.com>
2020-09-07 11:21:40 -0300

    feat: exporta style dentro do js

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
     output: {
         filename: 'bundle.min.js',
         path: __dirname + '/public/'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader', // Adiciona CSS injetando a tag style
+                'css-loader' // interpreta @import, url()...
+            ]
+        }]
     }
 }

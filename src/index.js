897a3540334ade1dffa5c73fdc5bdc74eed7eccc
Moises Mariano <moisesduartem@gmail.com>
2020-09-07 11:49:12 -0300

    feat: importa a folha css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-const Pessoa = require('./Pessoa');
-
-// import Pessoa from './Pessoa';
+import Pessoa from './Pessoa';
+import './assets/css/styles.css';
 
 const atendente = new Pessoa;
 console.log(atendente.cumprimentar());

eccf09bc41aeb251d1f1df77502b4ae5e27df440
Moises Mariano <moisesduartem@gmail.com>
2020-09-09 19:44:00 -0300

    feat: test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HelloWorld from './components/HelloWorld';
+
+class App extends Component
+{
+    render()
+    {
+        return (
+            <HelloWorld />
+        );
+    }
+}
+
+export default App;
